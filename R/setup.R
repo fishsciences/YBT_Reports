7bed31b4292903ce7011acf63dd851b1279be918
Myfanwy <myfanwy@github.com>
2020-12-03 10:27:09 -0800

    final edits to 2019 report

--- R/setup.R
+++ R/setup.R
@@ -4,19 +4,21 @@
 #--------------------------------------------#
 # This script is Step 1 of the analysis workflow, after the database workflow is complete.  It loads necessary libraries, pulls data from the Yolo Bypass telemetry SQLite database and creates variables in the global environment which Step 2 (R/clean.R) and the reproducible reports require.
 #-------------------------------------------------------#
-library(dplyr)
-library(RCurl)
-library(RJSONIO)
-library(ybt)
-library(fishpals)
+# 
+# library(RCurl)
+# library(RJSONIO)
+
 
 # Global Variables
 #--------------------------------------------#
-# Browser handles for BARD query:
-curl = getCurlHandle(useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36", followlocation = TRUE)
+# base dropbox filepath:
+base_fp = "~/NonDropboxRepos/2020-YBT-FinalReport2019/data_clean/"
+
+#base_fp = "C:/Users/Annie Brodsky/Dropbox (Cramer Fish Sciences)/NewPROJECTS/AECCA-2018-YoloTelemetry/DELIVERABLES/Database/"
 
 # database connection: requires >= RSQLite 2.1.1
-db = RSQLite::dbConnect(RSQLite::SQLite(), "~/Dropbox (Cramer Fish Sciences)/NewPROJECTS/AECCA-2018-YoloTelemetry/WORKING/GoogleDriveBackup20190723/yb_database.sqlite")
+db_fp = paste0(base_fp, "ybt_database.sqlite") # filepath to the database
+#con = RSQLite::dbConnect(RSQLite::SQLite(), db_fp)
 
 # late-fall TagIDs to be excluded:
 latefalls <- c(31570, 13720, 13723)
@@ -25,32 +27,13 @@ latefalls <- c(31570, 13720, 13723)
 #--------------------------------------------#
 
 # Chinook tagging metadata (includes RAMP data)
-chn = tbl(db, "chn") %>% collect() %>% 
-  mutate(DateTagged = ymd(DateTagged))
-
+load(paste0(base_fp, "setup_cleaned_chn.rda"))
 
 # All tagging metadata (including both wst and chinook)
-alltags = tbl(db, "tags") %>% collect() %>% 
-  mutate(DateTagged = as.Date(DateTagged))
+load(paste0(base_fp, "setup_cleaned_tags.rda"))
 
-# Receiver deployments table
-deps = tbl(db, "deployments") %>% 
-  collect() %>% 
-  select(StationAbbOld, Station, Receiver, Start = DeploymentStart, End = DeploymentEnd) %>% 
-  mutate(Start = force_tz(ymd_hms(Start), "Pacific/Pitcairn"),
-         End = force_tz(ymd_hms(End), "Pacific/Pitcairn"))
-
-deps = deps[!is.na(deps$End), ] # filter out the ragged last cell
-
-
-# All detections
-dets <- tbl(db, "detections") %>% 
-  collect() %>% # this pulls entire table - may take awhile
-  filter(TagID %in% alltags$TagID) %>% # filter down to just our fish
-  mutate(DateTimePST = ymd_hms(DateTimePST)) %>% 
-  arrange(DateTimePST)
-
-dets$DateTimePST <- force_tz(dets$DateTimePST, "Pacific/Pitcairn")
+# All cleaned detections
+load(paste0(base_fp, "clean_dets_2019_report.rda"))
 
 #--------------------------------------------#
-RSQLite::dbDisconnect(db) # disconnect from database
+#RSQLite::dbDisconnect(db) # disconnect from database
