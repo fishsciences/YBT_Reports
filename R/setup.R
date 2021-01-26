d87078a34ae0fa8f8540df37a3226bac4d95fee2
Myfanwy <myfanwy@github.com>
2021-01-25 23:12:16 -0800

    update setup.R for new workflow; rm clean_focusyr.r

--- R/setup.R
+++ R/setup.R
@@ -1,39 +1,30 @@
 #--------------------------------------------#
 # Yolo Bypass Telemetry Project Setup Script
-# 2019-08-22 M. Johnston
+# 2020-01-26 M. Johnston
 #--------------------------------------------#
-# This script is Step 1 of the analysis workflow, after the database workflow is complete.  It loads necessary libraries, pulls data from the Yolo Bypass telemetry SQLite database and creates variables in the global environment which Step 2 (R/clean.R) and the reproducible reports require.
-#-------------------------------------------------------#
-# 
-# library(RCurl)
-# library(RJSONIO)
+# This script installs all the necessary packages for making the reproducible telemetry report
+
+CRAN.packages <- c("lubridate", 
+                   "knitr", 
+                   "ggplot2", 
+                   "dplyr", 
+                   "plyr", 
+                   "ggridges", 
+                   "devtools",
+                   "padr", 
+                   "tidyr",
+                   "ggbeeswarm")
+
+github.packages <- c("fishpals", "tagtales", "ybt", "CDECRetrieve")
+
+github.package_sites <- c("Myfanwy/fishpals", "Myfanwy/tagtales", "Myfanwy/ybt", "flowwest/CDECRetrieve")
 
 
-# Global Variables
-#--------------------------------------------#
-# base dropbox filepath:
-base_fp = "~/NonDropboxRepos/2020-YBT-FinalReport2019/data_clean/"
+new.packages <- CRAN.packages[!(CRAN.packages %in% installed.packages()[,"Package"])]
 
-#base_fp = "C:/Users/Annie Brodsky/Dropbox (Cramer Fish Sciences)/NewPROJECTS/AECCA-2018-YoloTelemetry/DELIVERABLES/Database/"
+if(length(new.packages)) install.packages(new.packages)
 
-# database connection: requires >= RSQLite 2.1.1
-db_fp = paste0(base_fp, "ybt_database.sqlite") # filepath to the database
-#con = RSQLite::dbConnect(RSQLite::SQLite(), db_fp)
+new.gh.pkgs <- github.packages[!(github.packages %in% installed.packages()[, "Package"])]
 
-# late-fall TagIDs to be excluded:
-latefalls <- c(31570, 13720, 13723)
-
-# Create copies of database tables for the global environment:
-#--------------------------------------------#
-
-# Chinook tagging metadata (includes RAMP data)
-load(paste0(base_fp, "setup_cleaned_chn.rda"))
-
-# All tagging metadata (including both wst and chinook)
-load(paste0(base_fp, "setup_cleaned_tags.rda"))
-
-# All cleaned detections
-load(paste0(base_fp, "clean_dets_2019_report.rda"))
-
-#--------------------------------------------#
-#RSQLite::dbDisconnect(db) # disconnect from database
+# if this triggers an update request, select 3: nothing
+if(length(new.gh.pkgs)) devtools::install_github(github.package_sites)
